58521811796b38d75c516bf2001be968b031e4fc
i4dudx <87536161+i4dudx@users.noreply.github.com>
2025-10-25 16:39:43 +0000

    Criei códigos de algoritmo

--- projeto2-teste/algoritmo.py
+++ projeto2-teste/algoritmo.py
@@ -1,1 +1,15 @@
+tarefas = []
 
+while True:
+    print("\n1. Adicionar  2. Listar  3. Sair")
+    op = input("Escolha: ")
+
+    if op == "1":
+        tarefas.append(input("Tarefa: "))
+    elif op == "2":
+        for i, t in enumerate(tarefas, 1):
+            print(f"{i}. {t}")
+    elif op == "3":
+        break
+    else:
+        print("Opção inválida!")
